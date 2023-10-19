6fbfb6ddd474d0476f725f328fb09fd2e32bd316
Torres <santitorres460@gmail.com>
2023-10-19 18:10:04 -0300

    Added custom tailwind class and new profile route

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Home from './views/Home';
+import { Home } from './views/Home';
+import { Profile } from './views/Profile';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Home />,
+	},
+	{
+		path: '/profile/:id',
+		element: <Profile />,
 	},
 ]);
 
