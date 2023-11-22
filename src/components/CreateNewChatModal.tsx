4e304353bbf8889e1376cf106eb7ca139f9ce25e
Torres <santitorres460@gmail.com>
2023-11-22 19:39:17 -0300

    Add CreateNewChatModal component

---
+++ src/components/CreateNewChatModal.tsx
@@ -0,0 +1,90 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { ChatCard } from './ChatCard';
+import { UserContext } from '../utils/context/UserProvider';
+import { Button } from './Button';
+
+interface CreateNewChatModalProps {}
+
+interface User {
+	username: string;
+	id: number;
+}
+
+export const CreateNewChatModal = ({}: CreateNewChatModalProps) => {
+	const userState = useContext(UserContext);
+
+	const [userSearchText, setUserSearchText] = useState('');
+	const [userSearchResults, setUserSearchResults] = useState<User[]>([]);
+	const [selectedUser, setSelectedUser] = useState(-1);
+
+	useEffect(() => {
+		const getUserSearchResults = async () => {
+			try {
+				const response = await axios.get<User[]>(
+					`http://localhost:3000/api/v1/users/search/${userSearchText}`,
+					{
+						withCredentials: true,
+					}
+				);
+				setUserSearchResults(
+					response.data.filter((user) => user.username !== userState?.user.name)
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		if (userSearchText === '') {
+			setSelectedUser(-1);
+			setUserSearchResults([]);
+			return;
+		}
+		getUserSearchResults();
+	}, [userSearchText]);
+
+	const handleCreateNewChat = async () => {};
+
+	// TODO FIX BUTTON STYLES
+	return (
+		<dialog id="my_modal_3" className="modal">
+			<div className="modal-box bg-slate-800 h-fit overflow-hidden">
+				<form method="dialog">
+					<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+						✕
+					</button>
+				</form>
+				<div className="flex flex-col gap-4 justify-center mt-2 mx-4">
+					<input
+						className="rounded-lg mx-3 my-2 p-1 px-4 shadow bg-slate-900 text-white placeholder:text-white/60"
+						type="text"
+						placeholder="Search for a user"
+						onChange={(e) => setUserSearchText(e.target.value)}
+					/>
+
+					<ul className="grid gap-1 h-32 overflow-y-scroll no-scrollbar mb-2">
+						{userSearchResults.map((user) => (
+							<li
+								key={user.id}
+								className={`${
+									selectedUser === user.id ? 'bg-slate-900/50' : 'bg-slate-900'
+								} rounded-lg p-2 shadow max-h-[3.9rem] cursor-pointer hover:bg-slate-900/50 duration-100`}
+								onClick={() => setSelectedUser(user.id)}
+							>
+								<ChatCard chatName={user.username} unreadMessages={0} />
+							</li>
+						))}
+					</ul>
+					<div className="flex justify-center">
+						<Button
+							text="Start a new chat"
+							textColor="text-white"
+							bgColor="bg-slate-700"
+							handleClick={handleCreateNewChat}
+							isDisabled={selectedUser === -1}
+						/>
+					</div>
+				</div>
+			</div>
+		</dialog>
+	);
+};
