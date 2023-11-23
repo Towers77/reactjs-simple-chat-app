e96c61623fe4a22b4aae82031009bd6c2b1f98e6
Torres <santitorres460@gmail.com>
2023-11-23 18:52:45 -0300

    Refactor ChatCard component to include latestMessage

--- src/components/ChatCard.tsx
+++ src/components/ChatCard.tsx
@@ -1,14 +1,19 @@
 export interface ChatCardProps {
 	chatName: string;
 	unreadMessages: number;
+	latestMessage?: string;
 }
 
-export const ChatCard = ({ chatName, unreadMessages }: ChatCardProps) => {
+export const ChatCard = ({
+	chatName,
+	unreadMessages,
+	latestMessage,
+}: ChatCardProps) => {
 	return (
 		<>
 			<div className="flex gap-2">
 				<svg
-					className="w-8 h-8 text-almond rounded-full m-1 mt-[0.4rem]"
+					className="w-8 h-8 text-slate-400 rounded-full m-1 mt-[0.4rem]"
 					aria-hidden="true"
 					xmlns="http://www.w3.org/2000/svg"
 					fill="currentColor"
@@ -16,10 +21,21 @@ export const ChatCard = ({ chatName, unreadMessages }: ChatCardProps) => {
 				>
 					<path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
 				</svg>
-				<span className="mt-[0.7rem] text-white">{chatName}</span>
+				<div className="flex flex-col">
+					<span
+						className={`text-white text-md ${
+							latestMessage ? '' : 'mt-[0.65rem]'
+						}`}
+					>
+						{chatName}
+					</span>
+					<span className="text-white/50 text-xs line-clamp-1">
+						{latestMessage}
+					</span>
+				</div>
 			</div>
 			{unreadMessages > 0 && (
-				<div className="badge mt-3 bg-almond border-none text-navy">
+				<div className="badge mt-3 bg-slate-300 border-none text-slate-800">
 					{'+' + unreadMessages}
 				</div>
 			)}
