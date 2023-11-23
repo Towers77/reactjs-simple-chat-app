7336a3533cb49c46bb22f46df1ba8b3f25846a82
Torres <santitorres460@gmail.com>
2023-11-23 18:52:32 -0300

    Refactor Profile component and update user state

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,20 +1,13 @@
 import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-
-import { UserContext } from '../utils/context/UserProvider';
-
-import { ChatCard } from '../components/ChatCard';
+import { User, UserContext } from '../utils/context/UserProvider';
 import { CreateNewChatModal } from '../components/CreateNewChatModal';
-
-interface Profile {
-	username: string;
-}
+import { ChatList } from '../components/ChatList';
 
 export const Profile = () => {
 	const userState = useContext(UserContext);
 	const [isLoading, setIsLoading] = useState(false);
-	const [chatSearchText, setChatSearchText] = useState('Search for a chat');
 
 	const { id } = useParams();
 
@@ -23,14 +16,15 @@ export const Profile = () => {
 	useEffect(() => {
 		const getProfile = async () => {
 			try {
-				const response = await axios.get<Profile>(
+				const response = await axios.get<User>(
 					`http://localhost:3000/api/v1/users/${id}`,
 					{
 						withCredentials: true,
 					}
 				);
 
-				userState?.setName(response.data.username);
+				userState?.setUsername(response.data.username);
+				userState?.setId(response.data.id);
 			} catch (error) {
 				console.log(error);
 			}
@@ -50,6 +44,8 @@ export const Profile = () => {
 			setIsLoading(true);
 			if (response.status !== 200) throw new Error('Something went wrong...');
 
+			userState?.setUsername('');
+			userState?.setId(0);
 			setTimeout(() => navigate('/'), 1000);
 		} catch (error) {
 			console.log(error);
@@ -71,7 +67,7 @@ export const Profile = () => {
 			) : (
 				<main className="flex bg-slate-900 w-full h-full z-50 shadow-lg 2xl:w-[1500px] shadow-black/50">
 					<CreateNewChatModal />
-					<aside className="basis-1/4 flex flex-col ">
+					<aside className="basis-1/4 w-96 flex flex-col ">
 						<div className="bg-slate-800 flex justify-between p-3">
 							<div className="flex gap-2">
 								<svg
@@ -84,7 +80,7 @@ export const Profile = () => {
 									<path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
 								</svg>
 								<span className="text-white text-lg mt-[0.60rem]">
-									{userState?.user.name}
+									{userState?.user.username}
 								</span>
 							</div>
 							<details className="dropdown dropdown-end">
@@ -111,34 +107,7 @@ export const Profile = () => {
 								</ul>
 							</details>
 						</div>
-						<div className="grid">
-							<svg
-								className="w-4 h-4 text-white absolute m-4 ml-6"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 20 20"
-							>
-								<path
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-								/>
-							</svg>
-							<input
-								className="rounded-lg mx-3 my-2 p-1 pl-10 bg-slate-800 text-white placeholder:text-white/60"
-								type="text"
-								placeholder="Search for a chat"
-								onChange={(e) => setChatSearchText(e.target.value)}
-							/>
-							<ul className="grid">
-								<li className="bg-slate-900 shadow p-2 flex hover:bg-slate-800 duration-75 justify-between">
-									<ChatCard chatName="Franco" unreadMessages={0} />
-								</li>
-							</ul>
-						</div>
+						<ChatList />
 					</aside>
 					<aside className="basis-3/4">lol</aside>
 				</main>
