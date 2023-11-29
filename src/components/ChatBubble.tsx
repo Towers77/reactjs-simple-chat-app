c53aef2ac0bfee9f1a8b679bd38a8ce22915849b
Torres <santitorres460@gmail.com>
2023-11-29 01:08:19 -0300

    Fix time display in ChatBubble component

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -13,17 +13,23 @@ export const ChatBubble = ({
 }: ChatBubbleProps) => {
 	const bgColor = type === 'chat-end' ? 'bg-slate-600' : 'bg-slate-800';
 
+	const auxTime = new Date(sentAt as Date);
+
+	const time = new Date(auxTime.setHours(auxTime.getHours() - 3));
+
 	return (
 		<div className={`chat ${type}`}>
 			<div
-				className={`chat-bubble flex place-items-center text-white ${bgColor} shadow-md max-w-1/2`}
+				className={`chat-bubble text-white ${bgColor} break-words shadow-md max-w-1/2`}
 			>
 				{message}
 			</div>
-			{sender && <div className="chat-header text-md text-white">{sender}</div>}
+			{sender && (
+				<div className="chat-header text-md text-white mb-1">{sender}</div>
+			)}
 			{sentAt && (
 				<div className="chat-footer text-sm opacity-50 mt-1 text-white/50">
-					{sentAt.toLocaleTimeString().slice(0, 5)}
+					{time.toLocaleTimeString().slice(0, 4)}
 				</div>
 			)}
 		</div>
